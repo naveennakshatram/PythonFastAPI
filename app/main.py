45ca61c80cadfe2a7c08b19f93c62cc876555232
Nakshatram Naveen Kumar <mailmenaveenkumar@gmail.com>
2025-07-24 14:06:12 +0100

    release: ok

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello from FastAPI on Kubernetes!"}
+    return {"message": "Hello from FastAPI on Kubernetes by Naveen!"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
